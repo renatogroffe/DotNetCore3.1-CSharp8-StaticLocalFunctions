869ce5c7a2c24cad977715c3278e81373d61b8ec
Renato Groffe <renato.groffe@yahoo.com.br>
2020-01-05 22:24:33 -0300

    .NET Core 3.1 + Console Application + C #8.0 + Static Local Functions
    .NET Core 3.1 + Console Application + C #8.0 + Static Local Functions

---
+++ ExemploStaticLocalFunctions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExemploStaticLocalFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculando a raiz cúbica de alguns números...");
+            Console.WriteLine($"8 => {RaizCubica(8)}");
+            Console.WriteLine($"27 => {RaizCubica(27)}");
+            Console.WriteLine($"64 => {RaizCubica(64)}");
+
+            static double RaizCubica(double valor) =>
+                Math.Round(Math.Pow(valor, 1.0 / 3.0), 2);
+        }
+    }
+}
